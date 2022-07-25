9d3240a60a4a2c5cdb436780f46b77afb222983d
fumixer <fumixer@gmail.com>
2022-07-25 18:45:48 +0900

    update pass

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const db = mysql.createConnection(
     // Your MySQL username,
     user: 'root',
     // Your MySQL password
-    password: `MikaHappy2022`,
+    password: mypass,
     database: 'election'
   }
 );
